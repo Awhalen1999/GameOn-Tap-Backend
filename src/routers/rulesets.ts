8443fd6e3e848172f9a0640608182f048d0f3a59
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-05-01 23:19:06 -0230

    1

--- src/routers/rulesets.ts
+++ src/routers/rulesets.ts
@@ -27,7 +27,7 @@ RulesetsRouter.put(
 // Route to create a new ruleset for a specific user and game
 RulesetsRouter.post('/:userId/:gameId/rulesets', ruleHandlers.createRuleset);
 
-// Route to delete a specific ruleset for a specific user and game
+// Route to delete a specific ruleset for a specific user and game (changed ruleset data to be let instead of const)
 RulesetsRouter.delete(
   '/:userId/:gameId/rulesets/:rulesetId',
   ruleHandlers.deleteRuleset
