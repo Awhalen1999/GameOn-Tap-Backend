c0008a0cb22a9d6e1f36bf5f91f30dde70f87596
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-06-10 15:30:12 -0230

    req password

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,5 +1,5 @@
 import db from './db';
-import { User, UserWithoutPassword } from '../types';
+import { User } from '../types';
 
 export async function LoginUser(
   email: string,
@@ -22,18 +22,27 @@ export async function LoginUser(
   }
 }
 
-export async function SignupUser(user: User): Promise<UserWithoutPassword> {
-  const users = await db`
-    INSERT INTO users
-      (email, username, password, theme)
-    VALUES
-      (${user.email}, ${user.username}, ${user.password}, ${user.theme})
-    RETURNING user_id, email, username, theme
+export async function SignupUser(user: User): Promise<User> {
+  const existingUsers = await db`
+    SELECT * FROM users
+    WHERE email = ${user.email} OR username = ${user.username}
   `;
-  return {
-    user_id: users[0].user_id as number,
-    email: users[0].email as string,
-    username: users[0].username as string,
-    theme: users[0].theme as string,
-  };
+  if (existingUsers.length > 0) {
+    throw new Error('A user with this email or username already exists');
+  } else {
+    const users = await db`
+      INSERT INTO users
+        (email, username, password, theme)
+      VALUES
+        (${user.email}, ${user.username}, ${user.password}, ${user.theme})
+      RETURNING user_id, email, username, theme
+    `;
+    return {
+      user_id: users[0].user_id as number,
+      email: users[0].email as string,
+      username: users[0].username as string,
+      theme: users[0].theme as string,
+      password: users[0].password as string,
+    };
+  }
 }
