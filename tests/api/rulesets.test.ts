ba09de051bd8274b1e66e747f159d93da75a38d2
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-07-03 19:34:14 -0230

    getruleset mock

--- tests/api/rulesets.test.ts
+++ tests/api/rulesets.test.ts
@@ -1,10 +1,11 @@
 import { afterEach, describe, expect, test, vi } from 'vitest';
-import { getRulesets } from '../../src/api/rulesets';
+import { getRulesets, getRuleset } from '../../src/api/rulesets';
 import { Ruleset } from '../../src/types';
 
 const mocks = vi.hoisted(() => {
   return {
     getRulesets: vi.fn(),
+    getRuleset: vi.fn(),
   };
 });
 
@@ -14,6 +15,7 @@ vi.mock('../../src/data/rulesets', async (importOriginal) => {
   return {
     ...mod,
     getRulesets: mocks.getRulesets,
+    getRuleset: mocks.getRuleset,
   };
 });
 
@@ -37,5 +39,32 @@ describe('getRulesets()', () => {
     expect(result).toEqual(data);
     expect(mocks.getRulesets).toHaveBeenCalledOnce();
     expect(mocks.getRulesets).toHaveBeenLastCalledWith(0, '');
+  });
+});
+
+describe('getRuleset()', () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  test('Should return a ruleset if it exists', async () => {
+    // 1. Setup the environment
+    const data: Ruleset = {
+      user_id: 0,
+      game_id: '',
+      name: '',
+      rules: {},
+      ruleset_id: 0,
+    };
+
+    mocks.getRuleset.mockResolvedValueOnce(data);
+
+    // 2. Execute code
+    const result = await getRuleset(0, '', 0);
+
+    // 3. Assert results and expectations
+    expect(result).toEqual(data);
+    expect(mocks.getRuleset).toHaveBeenCalledOnce();
+    expect(mocks.getRuleset).toHaveBeenLastCalledWith(0, '', 0);
   });
 });
